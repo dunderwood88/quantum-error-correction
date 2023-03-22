484432f7ca8d1d2cf7ba7c2b56082b8aef2a54e7
Dan <dan.underwood88@gmail.com>
2023-03-22 12:49:49 +0000

    x parity checks completed

--- quantum/error_correction/codes/rotated_planar.py
+++ quantum/error_correction/codes/rotated_planar.py
@@ -1,5 +1,5 @@
 import math
-from typing import List, Union
+from typing import List, Tuple, Union
 
 
 class RPlanarCode:
@@ -7,6 +7,7 @@ class RPlanarCode:
         D: data qubits
         X: X-type stabilizer qubits
         Z: Z-type stabilizer qubits
+    Indices run from left-to-right, top-to-bottom, for all qubit types.
 
     Example for dimension = 3
     (a.k.a. Surface-17 https://arxiv.org/pdf/1612.08208.pdf)
@@ -49,13 +50,14 @@ class RPlanarCode:
             "z": []
         }
 
-        # Z-type parity checks
+        # initial Z-type parity checks
         p_check_weight_2 = (1 << self._dimension) + 1
         p_check_weight_4 = ((3 << self._dimension) + 3) << 1
         
         p_temp = 0
         for p in range(self._num_stabilizer_qubits):
 
+            # weight-2 parity checks
             if p_temp % self._dimension == 0:
                 self._parity_checks["z"].append(p_check_weight_2)
                 if p_temp == 0:
@@ -66,87 +68,103 @@ class RPlanarCode:
                     p_check_weight_2 = p_check_weight_2 << 1
                     p_check_weight_4 = p_check_weight_4 << 2
                     p_temp = 0
-            else:
+            else:  # weight-4 parity checks
                 self._parity_checks["z"].append(p_check_weight_4)
                 p_check_weight_4 = p_check_weight_4 << 2
                 p_temp += 1
 
-        # X-type parity checks
+        # initial X-type parity checks
         p_check_weight_2 = 6
         p_check_weight_4 = (3 << self._dimension) + 3
-        
+
         p_temp = 0
         for p in range(self._num_stabilizer_qubits):
 
-            if p_temp % self._dimension == 0:
-                self._parity_checks["z"].append(p_check_weight_2)
-                if p_temp == 0:
-                    p_check_weight_2 = \
-                        p_check_weight_2 << (2 * self._dimension) - 1
-                    p_temp += 1
-                elif p_temp == self._dimension:
-                    p_check_weight_2 = p_check_weight_2 << 1
-                    p_check_weight_4 = p_check_weight_4 << 2
-                    p_temp = 0
-            else:
-                self._parity_checks["z"].append(p_check_weight_4)
+            # weight-4 parity checks
+            if p >= (self._dimension - 1) / 2 and \
+                p < (self._dimension * (self._dimension - 1)) / 2:
+                self._parity_checks["x"].append(p_check_weight_4)
                 p_check_weight_4 = p_check_weight_4 << 2
                 p_temp += 1
+                if p_temp == (self._dimension - 1) / 2:
+                    p_check_weight_4 = p_check_weight_4 << 2
+                elif p_temp == self._dimension - 1:
+                    p_temp = 0
+            else:  # weight-2 parity checks
+                self._parity_checks["x"].append(p_check_weight_2)
+                if p == ((self._dimension - 1) / 2) - 1:
+                    p_check_weight_2 = \
+                        p_check_weight_2 << ((self._dimension - 1)**2 + 1)
+                else:
+                    p_check_weight_2 = p_check_weight_2 << 2
 
-    # def apply_error(self, index: int, error_type: str="x"):
-
-    #     if index > self._dimension**2 - 1:
-    #         raise ValueError(f"Index {index} higher than number of data qubits!")
-
-    #     if error_type not in self._error_data_qubits.keys():
-    #         raise ValueError(
-    #             f"Error type must be one of {list(self._error_data_qubits.keys())}"
-    #         )
-
-    #     # update the errored data qubits
-    #     if index not in self._error_data_qubits[error_type]:
-    #         self._error_data_qubits[error_type].append(index)
-    #     else:
-    #         self._error_data_qubits[error_type].remove(index)
-
-    #     # update the active syndrome qubits
-    #     syndrome_type = next(
-    #         s for s in list(self._active_syndrome_qubits.keys()) if s != error_type
-    #     )
-
-    #     data_row, data_col = self._get_qubit_row_column(index, "d")
-
-    # def _get_qubit_row_column(self, index: int, type: str="z") -> (int, int):
+    def _get_qubit_row_column(
+        self, index: int, type: str="z"
+    ) -> Tuple[int, int]:
         
-    #     if (type == "x" or type == "z") and \
-    #         (index > self._num_stabilizer_qubits - 1):
-    #         raise ValueError("Index greater than number of stabilizer qubits!")
+        if (type == "x" or type == "z") and \
+            (index > self._num_stabilizer_qubits - 1):
+            raise ValueError("Index greater than number of stabilizer qubits!")
 
 
-    #     if type == "z":
-    #         dim = math.ceil(self._dimension / 2)
-    #     elif type == "x":
-    #         dim = math.floor(self._dimension / 2)
-    #     elif type == "d":
-    #         dim = self._dimension
+        if type == "z":
+            dim = math.ceil(self._dimension / 2)
+        elif type == "x":
+            dim = math.floor(self._dimension / 2)
+        elif type == "d":
+            dim = self._dimension
 
-    #     else:
-    #         raise ValueError("Invalid qubit type!")
+        else:
+            raise ValueError("Invalid qubit type!")
 
-    #     return math.floor(index / dim), index % dim
+        return math.floor(index / dim), index % dim
 
-    def _convert_list_to_int(self, error_string: List[int]) -> int:
-                num = 0
-                for i in error_string:
-                    num += (1 << i)
-                return num
+    def _convert_qubit_list_to_binary(self, error_string: List[int]) -> int:
+        """Takes a list of marked qubit inidices and converts to a binary
+        representation where unmarked qubit indices are set to 0, marked set
+        to 1.
+
+        Parameters
+        ----------
+        error_string : List[int]
+            List of marked qubit indices
+
+        Returns
+        -------
+        int
+            Binary representation of unmarked and marked qubit indices
+        """
+        num = 0
+        for i in error_string:
+            num += (1 << i)
+        return num
 
     def generate_syndrome(
         self, error_string: Union[int, List[int]], error_type: str="x"
     ) -> int:
+        """Given a data qubit error string in binary or listed qubit index 
+        form, and the error type, generate a syndrome string in binary form for
+        the corresponding syndrome qubits.
+        
+        error_type = "x" -> z-type syndrome string
+        error_type = "z" -> x-type syndrome string
+
+        Parameters
+        ----------
+        error_string : Union[int, List[int]]
+            The data qubit error string, given in binary form or as a list of
+            errored data qubit indices
+        error_type : str, optional
+            _description_, by default "x"
+
+        Returns
+        -------
+        int
+            _description_
+        """
 
         if isinstance(error_string, List):
-            error_string = self._convert_list_to_int(error_string)
+            error_string = self._convert_qubit_list_to_binary(error_string)
 
         syndrome_type = next(s for s in ["x", "z"] if s != error_type)
 
@@ -175,11 +193,15 @@ class RPlanarCode:
         z_syndrome = 0
         if x_error_string:
             if isinstance(x_error_string, List):
-                x_error_string = self._convert_list_to_int(x_error_string)
+                x_error_string = self._convert_qubit_list_to_binary(
+                    x_error_string
+                )
             z_syndrome = self.generate_syndrome(x_error_string, error_type="x")
         if z_error_string:
             if isinstance(z_error_string, List):
-                z_error_string = self._convert_list_to_int(z_error_string)
+                z_error_string = self._convert_qubit_list_to_binary(
+                    z_error_string
+                )
             x_syndrome = self.generate_syndrome(z_error_string, error_type="z")
 
         x = 0
@@ -188,7 +210,9 @@ class RPlanarCode:
 
         # first X qubits
         for i in range(math.floor(self._dimension / 2)):
-            str_code += "X" + "{:<15}".format(i)
+            if (1 << x) & x_syndrome:
+                str_code += "\033[92m"
+            str_code += "X" + "{:<15}".format(i) + "\033[0m"
             x += 1
         str_code += "\n" + "{:>4}".format("")
 
@@ -234,7 +258,9 @@ class RPlanarCode:
 
         # last X qubits
         for i in range(x, self._num_stabilizer_qubits):
-            str_code += "X" + "{:<15}".format(i)
+            if (1 << i) & x_syndrome:
+                str_code += "\033[92m"
+            str_code += "X" + "{:<15}".format(i) + "\033[0m"
         str_code += "\n"
 
         
@@ -253,5 +279,9 @@ class RPlanarCode:
 if __name__ == "__main__":
 
     code = RPlanarCode(7)
-    code.draw(x_error_string=1090519040, z_error_string=256)
-    code.draw(x_error_string=[19, 30, 36], z_error_string=[8])
+    # code.draw(x_error_string=69793742848, z_error_string=4294967552)
+    # code.draw(x_error_string=[19, 30, 36], z_error_string=[32, 8])
+    code.draw(z_error_string=[1, 47, 43], x_error_string=[7, 21, 28, 20, 34, 41])
+
+    # code = RPlanarCode(5)
+    # code.draw(z_error_string=[12, 18], x_error_string=[12])
